91eb2c02679f3994e95703cf783362f875d628c9
rahul yadav <rahulyadav170923@gmail.com>
2017-02-13 00:04:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 CONFIG = {
     'FACEBOOK_TOKEN': os.environ['FACEBOOK_TOKEN'],
     'VERIFY_TOKEN': 'shoptalk',
-    'SERVER_URL': 'http://e8213010.ngrok.io'
+    'SERVER_URL': 'https://shoppbot.herokuapp.com/'
 }
